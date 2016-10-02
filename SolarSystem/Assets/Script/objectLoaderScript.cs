d687fe53efadca18280e442d584c0af9c7ce637f
Noa <noamergi@gmail.com>
2016-10-01 22:42:02 -0400

    no building is colliding with the sphere

--- SolarSystem/Assets/Script/objectLoaderScript.cs
+++ SolarSystem/Assets/Script/objectLoaderScript.cs
@@ -13,17 +13,36 @@ public static class objectLoaderScript
         for (int i = 0; i < dencity; ++i)
         {
             GameObject objectToBeSpawned = choicesList[rand.Next(0, choicesList.Count - 1)];
-            //float y = objectToBeSpawned.GetComponent<Renderer>().bounds.size.y;
-            Vector3 pos = Random.onUnitSphere * parent.GetComponent<SphereCollider>().radius + parent.transform.position;
-            Debug.Log(pos);
+
+            BoxCollider objBounds = objectToBeSpawned.GetComponent<BoxCollider>();
+
+            Vector3 pos = getEmpteyPos(parent, objBounds);
 
             GameObject obj = Object.Instantiate(objectToBeSpawned, pos, Quaternion.identity, parent.transform) as GameObject;
 
-
-            //obj.transform.position = pos;
             obj.transform.LookAt(parent.transform);
             obj.transform.Rotate(-90, 0, 0);
+
+
+            container.Add(obj);
         }
+
+        
+    }
+    
+    private static Vector3 getEmpteyPos(GameObject parent, BoxCollider objBounds)
+    {
+        Vector3 pos;
+        Collider[] objectsColidedWith;
+
+        do
+        {
+            pos = Random.onUnitSphere * parent.GetComponent<SphereCollider>().radius + parent.transform.position;
+            objectsColidedWith = Physics.OverlapBox(new Vector3(pos.x, pos.y + (objBounds.size.y / 2), pos.z), objBounds.size / 2, Quaternion.identity);
+
+        } while (objectsColidedWith.Length > 1);
+        return pos;
     }
 
+   
 }
